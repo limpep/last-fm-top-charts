d126e2e359a972d20511c17dca795b7ffc51683b
Ergun Polat <ergunpolat@msn.com>
2020-04-25 13:34:42 +0100

    custom tool bar

--- app/src/main/java/com/example/lastfmtopcharts/view/MainActivity.kt
+++ app/src/main/java/com/example/lastfmtopcharts/view/MainActivity.kt
@@ -3,55 +3,46 @@ package com.example.lastfmtopcharts.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.example.lastfmtopcharts.R
 import com.example.lastfmtopcharts.viewmodel.MainViewModel
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class MainActivity : AppCompatActivity() {
 
 //    lateinit var viewModel: MainViewModel
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val layout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar_layout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
         navController = Navigation.findNavController(this, R.id.fragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
-
-
-//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
-//        getTopArtistChart()
-
-
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        layout.setupWithNavController(toolbar, navController, appBarConfiguration)
 
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, null)
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
-
-
-
-//    fun getTopArtistChart() {
-//        viewModel.getTopArtistChart.observe(this, Observer {
-//            for (artist in it.artists.artist) {
-//                Log.d(this.packageName, artist.name)
-//                Log.d(this.packageName, artist.mbid)
-//                Log.d(this.packageName, artist.playcount)
-//                for (images in artist.image) {
-//                    Log.d(this.packageName, images.imageURL)
-//                }
-//            }
-//        })
-//    }
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
